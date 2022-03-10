ac29e07b1c0d24a30219c726d6b77ed209b3d052
mohakchethan <mohakchethan@gmail.com>
2022-03-10 16:29:40 +0530

    add function added

--- add.js
+++ add.js
@@ -1,5 +1,9 @@
-var a
-var b
+func add() {
+var a = documnet.getElementById("num1").value
+var b = document.getElementById("num2").value
 var result
 result = a+b
+}
+
+document.getElementById("result").value = result
 
