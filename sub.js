d98dc717f64d15d69db3d0b7f27c7b530e8d0546
chethanmk666 <chethanmk@666>
2022-03-10 16:31:35 +0530

    substraction modified

--- sub.js
+++ sub.js
@@ -1,5 +1,8 @@
-var a
-var b
+function sub()
+{
+var a= document.getElementById("num1").value
+var b = document.getElementById("num2").value
 var result
 result=a-b
-
+docuemnt.getElementById("result").value = result
+}
