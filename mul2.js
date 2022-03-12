048f84d8e854e580e8f7c52cdc18c506174a1104
mohakchethan <mohakchethan99@gmail.com>
2022-03-12 10:42:13 +0530

    multiply file modified

--- mul2.js
+++ mul2.js
@@ -1,1 +1,9 @@
+func mul() {
+var a = documnet.getElementById("num1").value
+var b = document.getElementById("num2").value
+var result
+result = a*b
+}
+
+document.getElementById("result").value = result
 new mul file
